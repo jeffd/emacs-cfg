eddf5f3354e04ce992e546af76d4f2f5d27f5567
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2019-11-28 09:19:34 -0800

    Delete old modes

--- emacs.d/build-and-go.el
+++
@@ -1,3 +0,0 @@
-(defun build-and-go-in-xcode ()
-  (shell-command "osascript -e 'tell application \"Xcode\" to build project 1'")
-  (shell-command "osascript -e 'tell application \"Xcode\" to launch project 1'"))
